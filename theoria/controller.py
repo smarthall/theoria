8627b0e7e6ac2d219008345a6c025a1b0dcad6b8
Daniel Hall <daniel@danielhall.me>
2015-08-22 23:32:27 +1000

    Send a mandatory update ever 5 seconds

--- theoria/controller.py
+++ theoria/controller.py
@@ -5,6 +5,8 @@ The Theoria main controller.
 import threading
 from Queue import Queue, Empty
 
+from util import RepeatTimer
+
 class Controller(threading.Thread):
     def __init__(self, driver):
         threading.Thread.__init__(self)
@@ -12,8 +14,11 @@ class Controller(threading.Thread):
         self._driver = driver
         self._update_queue = Queue()
         self._done = False
+        self._timer = RepeatTimer(5, self._driver.send_buffer)
+        self._timer.daemon = True
 
     def run(self):
+        self._timer.start()
         while not self._done:
             try:
                 (args, kwargs) = self._update_queue.get(True, 1)
