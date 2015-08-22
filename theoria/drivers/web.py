e73d268aa64dda4e37e0cb08859f475a43f9308f
Daniel Hall <daniel@danielhall.me>
2015-08-22 22:51:41 +1000

    Added a WebSocket

--- theoria/drivers/web.py
+++ theoria/drivers/web.py
@@ -6,16 +6,28 @@ import cherrypy
 import threading
 from PIL import Image
 
+from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
+from ws4py.websocket import WebSocket
+
 def create(*args, **kwargs):
     return WebDriver(*args, **kwargs)
 
 def start_web():
-    cherrypy.quickstart(TheoriaWeb())
+    cherrypy.config.update({
+        'server.socket_port': 9000,
+        'engine.autoreload.on': False,
+    })
+    WebSocketPlugin(cherrypy.engine).subscribe()
+    cherrypy.tools.websocket = WebSocketTool()
+    cherrypy.quickstart(TheoriaWeb(), '/', config={'/ws': {
+        'tools.websocket.on': True,
+        'tools.websocket.handler_cls': TheoriaWebSocket
+    }})
 
 class WebDriver:
     def __init__(self):
         self._web_thread = threading.Thread(target=start_web)
-        self._web_thread.name = 'Theoria-Web'
+        self._web_thread.name = 'Theoria-WebDriver'
         self._web_thread.daemon = True
         self._web_thread.start()
         self._buffer = Image.new('RGBA', (1024, 600))
@@ -27,8 +39,39 @@ class WebDriver:
     def send_buffer(self):
         pass
 
+
 class TheoriaWeb:
     @cherrypy.expose
     def index(self):
-        return 'This is a test'
+        return """
+        <html>
+        <head>
+        <title>Theoria Web</title>
+        <script type="text/javascript">
+            var socket = new WebSocket('ws://localhost:9000/ws');
+            socket.onopen = function(){
+                console.log('WebSocket Open!');
+            }
+            socket.onclose = function(){
+               console.log('WebSocket Closed');
+            }
+            socket.onmessage = function(e){
+               var server_message = e.data;
+               console.log(server_message);
+            }
+        </script>
+        </head>
+        <body>
+        <h1>Theoria Web</h1>
+        </body>
+        </html>
+        """
+
+    @cherrypy.expose
+    def ws(self):
+        pass
+
+class TheoriaWebSocket(WebSocket):
+    def received_message(self, message):
+        self.send(message.data, message.is_binary)
 
