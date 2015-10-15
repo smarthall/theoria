070f43e55863806eceeb16f23908f8b5fc16ed45
Daniel Hall <daniel@danielhall.me>
2015-10-15 13:18:14 +1100

    A conditional screen, which right now is only based on time

--- theoria/screen/containers.py
+++ theoria/screen/containers.py
@@ -2,6 +2,7 @@ from base import BaseScreen
 from PIL import Image
 from theoria.util import RepeatTimer
 from theoria.exceptions import ConfigException
+from datetime import datetime
 
 DEFAULT_ROTATION_TIMER = 10
 
@@ -27,6 +28,46 @@ class ContainerScreen(BaseScreen):
     def child_changed(self):
         self.draw()
         self.changed()
+
+class ConditionalScreen(BaseScreen):
+    def __init__(self, buf, screens, true, false, condition, *args, **kwargs):
+        self._bufsize = buf.size
+
+        super(ConditionalScreen, self).__init__(buf=buf, screens=screens, *args, **kwargs)
+
+        # Make buffers
+        self._truebuf = Image.new('RGBA', self._bufsize)
+        self._falsebuf = Image.new('RGBA', self._bufsize)
+
+        # Make screens
+        truescreen = screens[true](screens=screens, buf=self._truebuf)
+        falsescreen = screens[false](screens=screens, buf=self._falsebuf)
+
+        # Subscribe to their changes
+        truescreen.subscribe(callback=self.child_changed)
+        falsescreen.subscribe(callback=self.child_changed)
+
+        # Compile the condition
+        self._condition = compile(condition, '<string>', 'eval')
+
+    def condition(self):
+        return eval(self._condition, {}, {
+            'now': datetime.now(),
+        })
+
+    def child_changed(self):
+        self.draw()
+        self.changed()
+
+    def draw(self):
+        result = self.condition()
+
+        if result:
+            cbuf = self._truebuf
+        else:
+            cbuf = self._falsebuf
+
+        self._buf.paste(cbuf, (0, 0), cbuf)
 
 class RotateScreen(ContainerScreen):
     def __init__(self, buf, refresh=DEFAULT_ROTATION_TIMER, *args, **kwargs):
@@ -90,4 +131,37 @@ class GridScreen(ContainerScreen):
             self._buf.paste(buf, (drawx, drawy), buf)
 
         self.changed()
+
+class ConditionalTimeScreen(ContainerScreen):
+    def __init__(self, buf, *args, **kwargs):
+        width, height = buf.size
+        self._bufsize = (width / int(cols), height / int(rows))
+        self._buffers = {}
+        self._cols = int(cols)
+        self._rows = int(rows)
+
+        super(GridScreen, self).__init__(buf=buf, *args, **kwargs)
+
+        if len(self._children) != (self._rows * self._cols):
+            raise ConfigException('Expected %s screens, but got %s.' % ((self._rows * self._cols), len(self._children)))
+
+        self.draw()
+
+    def make_buffer(self, num, name):
+        pos = (num / self._cols, num % self._cols)
+        newbuf = Image.new('RGBA', self._bufsize)
+        self._buffers[pos] = newbuf
+        return newbuf
+
+    def draw(self):
+        for pos, buf in self._buffers.iteritems():
+            r, c = pos
+            bufw, bufh = self._bufsize
+            drawx = c * bufw
+            drawy = r * bufh
+
+            self._buf.paste(buf, (drawx, drawy), buf)
+
+        self.changed()
+
 
